07ae243c10dc9927393f9713e7707cffd6b9255c
Jordy Villamil <jordyvillamilletras@gmail.com>
2025-06-12 10:48:29 -0500

    se verifico la conexion a la base de datos desde FastAPI, para esto se agregaron varios endpoints al archivo root

--- app/api/v1/root.py
+++ app/api/v1/root.py
@@ -1,7 +1,25 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
+from sqlalchemy.orm import Session
+from sqlalchemy import text
+from app.core.database import SessionLocal
 
 router = APIRouter()
+
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
 
 @router.get("/")
 async def read_root():
     return {"message": "¡Bienvenido a la API de Cibernodo!"}
+
+@router.get("/db-check")
+def db_check(db: Session = Depends(get_db)):
+    try:
+        db.execute(text("SELECT 1"))
+        return {"db_status": "ok"}
+    except Exception as e:
+        return {"db_status": "error", "detail": str(e)}
--- app/main.py
+++ app/main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI
 from app.api.v1 import root, auth
 
+
 app = FastAPI(
     title="Cibernodo API",
     version="0.1.0"
